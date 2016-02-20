153c66e63ec7ffbc66363053d7dbcdea06e43999
Yannick Lohse <contact@yannick-lohse.fr>
2016-02-20 22:50:27 +0100

    Scraping to disk

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ var bodyParser = require('body-parser');
 /*
     Configuration section.
 */
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static('client'));
 
 
@@ -18,9 +21,9 @@ app.use(archivesController);
 /*
     Start the HTTP server.
 */
-var server = app.listen(9250, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Listening at http://%s:%s', host, port);
+var server = app.listen(9250, function (){
+	var host = server.address().address;
+	var port = server.address().port;
+	
+	console.log('Listening at http://%s:%s', host, port);
 });
--- server/controllers/archives.js
+++ server/controllers/archives.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.status(200).sendFile('index.html');
+router.get('/', function(req, res, next){
+	res.status(200).sendFile('index.html');
+});
+
+router.post('/archive', function(req, res, next){
+	var url = req.body.url,
+		folder = url.match(/^https?:\/\/(\w+)\/?/)[1] + '-' + (new Date().getTime());
+    
+	var scraper = require('website-scraper');
+	var options = {
+		urls: [req.body.url],
+		directory: './scrap/' + folder,
+	};
+	
+	scraper.scrape(options).then(function(result){
+//		console.log(result);//[{url: 'url', filename: 'index.html'}]
+		console.log('done scraping');
+	}, function(err){
+		console.log(err);
+	});
+	
+	res.redirect('/');
 });
 
 // Export the router instance to make it available from other files.
