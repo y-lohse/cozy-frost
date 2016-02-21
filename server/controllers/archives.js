58369c0b089885df44c39e55d1bca66851ce968c
Yannick Lohse <contact@yannick-lohse.fr>
2016-02-21 13:18:57 +0100

    Tarballs to database+cleanup

--- server/controllers/archives.js
+++ server/controllers/archives.js
@@ -4,6 +4,7 @@ var WebPage = require('../models/webpage');
 var fs = require('fs');
 var path = require('path');
 var tar = require('tar-fs');
+var rimraf = require('rimraf');
 
 router.get('/archives', function(req, res, next){
 	WebPage.request('all', function(err, pages){
@@ -31,32 +32,63 @@ router.get('/archive/:id', function(req, res, next){
 
 router.post('/archive', function(req, res, next){
 	var url = req.body.url,
-		folder = url.match(/^https?:\/\/(\w+)\/?/)[1] + '-' + (new Date().getTime());
+		slug = url.match(/^https?:\/\/(\w+)\/?/)[1] + '-' + (new Date().getTime()),
+		scrapDestination = './scrap/' + slug,
+		tarball = './scrap/' + slug + '.tar';
 	
+	//prepare scraper
+	var scraper = require('website-scraper');
+	var options = {
+		urls: [req.body.url],
+		directory: scrapDestination,
+	};
+	
+	console.log('Preparing to scrap ' + url);
+	
+	//create DB object
 	WebPage.create({
 		'url': url
 	}, function(err, webpage){
 		if (err) next(err);
 		else{
-			console.log(webpage);
+			console.log('DB object created');
+			//everything is ok for the user, the rest will be async
 			res.redirect('/');
+			
+			//start actual scraping
+			scraper.scrape(options).then(function(result){
+				console.log('done scraping');
+				
+				//create a tarball
+				var writer = fs.createWriteStream(tarball);
+				var packer = tar.pack(scrapDestination).pipe(writer);
+
+				//then save the tarball to the db
+				//it seems like we can't directly pipe the tar packing to the db because the DB wants the total content length up front
+				writer.on('finish', function(){
+					var rstream = fs.createReadStream(tarball);
+
+					webpage.attachBinary(rstream, function(err) {
+						if (err) console.log(err);
+						else{
+							console.log(slug + 'tarball transfered to database');
+							//now we can clean up
+							fs.unlink(tarball, function(err){
+								if (err) console.log('error deleting tarball : ' + err);
+								else console.log(tarball + ' deleted');
+							});
+							rimraf(scrapDestination, function(err){
+								if (err) console.log('error deleting folder : ' + err);
+								else console.log(scrapDestination + ' deleted');
+							});
+						}
+					});
+				});
+			}, function(err){
+				console.log('error while scraping ' + url + ' : ' + err);
+			});
 		}
 	});
-    
-	var scraper = require('website-scraper');
-	var options = {
-		urls: [req.body.url],
-		directory: './scrap/' + folder,
-	};
-	
-	scraper.scrape(options).then(function(result){
-//		console.log(result);//[{url: 'url', filename: 'index.html'}]
-		console.log('done scraping');
-	}, function(err){
-		console.log(err);
-	});
-//	
-//	res.redirect('/');
 });
 
 // Export the router instance to make it available from other files.
