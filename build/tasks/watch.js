5f014bc4fd83c5b1ada1b44ee27820e4c214c079
Yannick Lohse <contact@yannick-lohse.fr>
2016-02-22 23:30:09 +0100

    break watcher

--- build/tasks/watch.js
+++ build/tasks/watch.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var paths = require('../paths');
-var browserSync = require('browser-sync');
+//var browserSync = require('browser-sync');
 
 // outputs changes to files to the console
 function reportChange(event) {
@@ -12,11 +12,11 @@ function reportChange(event) {
 // reportChange method. Also, by depending on the
 // serve task, it will instantiate a browserSync session
 gulp.task('watch', [], function() {
-  gulp.watch(paths.source, ['build-system', browserSync.reload]).on('change', reportChange);
-  gulp.watch(paths.html, ['build-html', browserSync.reload]).on('change', reportChange);
+  gulp.watch(paths.source, ['build-system']).on('change', reportChange);
+  gulp.watch(paths.html, ['build-html']).on('change', reportChange);
   gulp.watch(paths.css, ['build-css']).on('change', reportChange);
   gulp.watch(paths.style, function() {
-    return gulp.src(paths.style)
-      .pipe(browserSync.stream());
+//    return gulp.src(paths.style)
+//      .pipe(browserSync.stream());
   }).on('change', reportChange);
 });
