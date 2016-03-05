7f8883a909f5bb2cf9a6083d9f76d92e2ddfa9bd
Yannick Lohse <contact@yannick-lohse.fr>
2016-03-05 17:56:27 +0100

    Callback hell fix

--- server/controllers/archives.js
+++ server/controllers/archives.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var WebPage = require('../models/webpage');
 var fs = require('fs');
+var Q = require('Q');
 var path = require('path');
 var tar = require('tar-fs');
 var rimraf = require('rimraf');
@@ -44,67 +45,82 @@ router.delete('/archive/:id', function(req, res, next){
 });
 
 router.post('/archive', function(req, res, next){
-	console.log(req.body);
 	var url = req.body.url,
-		slug = url.match(/^https?:\/\/(\w+)\/?/)[1] + '-' + (new Date().getTime()),
+		slug = url.match(/^https?:\/\/(www\.)?(\w+)\/?/)[2] + '-' + (new Date().getTime()),
 		scrapDestination = './scrap/' + slug,
-		tarball = './scrap/' + slug + '.tar';
-	
-	//prepare scraper
-	var scraper = require('website-scraper');
-	var options = {
-		urls: [req.body.url],
-		directory: scrapDestination,
-	};
+		tarball = './scrap/' + slug + '.tar',
+		webpage = null;
 	
 	console.log('Preparing to scrap ' + url);
 	
-	//create DB object
-	WebPage.create({
+	Q.ninvoke(WebPage, 'create', {
 		'url': url,
 		'slug': slug
-	}, function(err, webpage){
-		if (err) next(err);
-		else{
-			console.log('DB object created');
-			//everything is ok for the user, the rest will be async
-			res.status(200).send();
-			
-			//start actual scraping
-			scraper.scrape(options).then(function(result){
-				console.log('done scraping');
-				
-				//create a tarball
-				var writer = fs.createWriteStream(tarball);
-				var packer = tar.pack(scrapDestination).pipe(writer);
-
-				//then save the tarball to the db
-				//it seems like we can't directly pipe the tar packing to the db because the DB wants the total content length up front
-				writer.on('finish', function(){
-					var rstream = fs.createReadStream(tarball);
-
-					webpage.attachBinary(rstream, function(err) {
-						if (err) console.log(err);
-						else{
-							console.log(slug + 'tarball transfered to database');
-							//now we can clean up
-							fs.unlink(tarball, function(err){
-								if (err) console.log('error deleting tarball : ' + err);
-								else console.log(tarball + ' deleted');
-							});
-							rimraf(scrapDestination, function(err){
-								if (err) console.log('error deleting folder : ' + err);
-								else console.log(scrapDestination + ' deleted');
-							});
-						}
-					});
-				});
-			}, function(err){
-				console.log('error while scraping ' + url + ' : ' + err);
-			});
-		}
-	});
+	})
+	.then(function(webpageInstance){
+		console.log('DB object created');
+		
+		webpage = webpageInstance;
+		
+		//everything is ok for the user, the rest will be async
+		res.status(200).send();
+		
+		//prepare scraper
+		var scraper = require('website-scraper');
+		
+		var options = {
+			urls: [url],
+			directory: scrapDestination,
+		};
+		
+		return scraper.scrape(options);
+	})
+	.then(function(result){
+		console.log('Finished scraping');
+		console.log(result);
+		
+		return packWebPage(tarball, scrapDestination);
+	})
+	.then(function(){
+		var rstream = fs.createReadStream(tarball);
+		
+		return Q.ninvoke(webpage, 'attachBinary', rstream);
+	})
+	.then(function(){
+		console.log(slug + 'tarball transfered to database');
+		
+		return Q.nfcall(fs.unlink, tarball);
+	})
+	.then(function(){
+		console.log(tarball + ' deleted');
+		
+		return Q.nfcall(rimraf, scrapDestination);
+	})
+	.then(function(){
+		console.log(scrapDestination + ' deleted');
+	})
+	.done();
 });
+
+function packWebPage(tarball, scrapDestination){
+	var def = Q.defer();
+	
+	//create a tarball
+	var writer = fs.createWriteStream(tarball);
+	var packer = tar.pack(scrapDestination).pipe(writer);
+	
+	writer.on('finish', function(){
+		def.resolve();
+	});
+	
+	writer.on('error', function(err){
+		def.reject(err);
+	});
+	
+	return def.promise;
+}
+
+
 
 // Export the router instance to make it available from other files.
 module.exports = router;
