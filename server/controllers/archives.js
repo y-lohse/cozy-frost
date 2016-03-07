db1b4acd67d261f8d626ca6793d76ea2cf15749d
Yannick Lohse <contact@yannick-lohse.fr>
2016-03-07 09:47:09 +0100

    require q lowercase

--- server/controllers/archives.js
+++ server/controllers/archives.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var WebPage = require('../models/webpage');
 var fs = require('fs');
-var Q = require('Q');
+var Q = require('q');
 var tar = require('tar-fs');
 
 router.get('/archives', function(req, res, next){
