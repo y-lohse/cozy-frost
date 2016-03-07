cddc148860bc422f5d2a8d0890d91dcec0e5d6a2
Yannick Lohse <contact@yannick-lohse.fr>
2016-03-07 23:32:42 +0100

    Add credentials #4

--- client/bm.js
+++ client/bm.js
@@ -1,17 +1,17 @@
 (function(){
 	var url = window.location.href,
 		scriptSrc = document.currentScript.getAttribute('src'),
-		api = scriptSrc.substr(0, scriptSrc.lastIndexOf('/')) + '/public/test';
+		api = scriptSrc.substr(0, scriptSrc.lastIndexOf('/')) + '/archive';
 	
 	if (self.fetch){
 		var myHeaders = new Headers();
 		myHeaders.append('Content-Type', 'application/json');
 		
 		fetch(api, {
-			method: 'get',
+			method: 'post',
 			headers: myHeaders,
-			redirect: 'follow',
 			mode: 'no-cors',
+			credentials: 'include',
 			body: JSON.stringify({
 				url: url
 			})
--- server/controllers/archives.js
+++ server/controllers/archives.js
@@ -42,10 +42,6 @@ router.delete('/archive/:id', function(req, res, next){
 	});
 });
 
-router.get('/public/test', function(req, res, next){
-	res.send('ok');
-});
-
 router.post('/archive', function(req, res, next){
 	var url = req.body.url,
 		slug = url.match(/^https?:\/\/(www\.)?(\w+)\/?/)[2] + '-' + (new Date().getTime()),
