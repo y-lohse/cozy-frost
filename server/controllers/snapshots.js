d6ac24dbfb86356033b4a4ddcf3e61e2aa8e1b6b
Yannick Lohse <contact@yannick-lohse.fr>
2016-10-15 13:33:27 +0200

    Fix #26

--- server/controllers/snapshots.js
+++ server/controllers/snapshots.js
@@ -97,6 +97,11 @@ router.post('/snapshot', function(req, res, next){
 		var options = {
 			urls: [url],
 			directory: scrapDestination,
+			request: {
+				headers: {
+					'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+				},
+			},
 		};
 		
 		//start scraping
